e9ec902170cc344fae49d079c768168a50b2a0c4
Saranya Karthikeyan <saranya.karthik@outlook.com>
2024-10-19 13:29:27 +0200

    Display flag and capital name fron end point

--- src/components/CountryModal.js
+++ src/components/CountryModal.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "../CSS/CountryModal.css"; // You can style the modal here
 
-const CountryModal = ({ show, onClose, country }) => {
+const CountryModal = ({ show, onClose, country,countryDetails,error }) => {
   if (!show || !country) return null;
-  // fetch('https://restcountries.com/v3.1/all')
-  // .then(response => response.json())
-  // .then(data => {
-  //   data.forEach(country => {
-  //     console.log(`Name: ${country.name.common}`);
-  //     console.log(`Capital: ${country.capital ? country.capital[0] : 'N/A'}`);
-  //     console.log(`Flag: ${country.flags.png}`);
-  //   });
-  // });
+
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>{country.properties.name}</h2>
-        <p><strong>Capital:</strong> {country.properties.continent || "N/A"}</p>
-        {/* <p><strong>Population:</strong> {country.properties.pop_est.toLocaleString()}</p> */}
-        <img src="https://flagcdn.com/in.svg" alt="My Icon" width={350} height={350} />
+      {countryDetails?(<>
+        <h2>{countryDetails?.name?.common}</h2>
+        <p><strong>Capital:</strong> {countryDetails?.capital[0] || "N/A"}</p>
+        <img src={countryDetails?.flags.png} alt="My Icon" width={350} height={350} /> </>
+      ):(<p>{error}  </p>)}
         <button onClick={onClose}>Close</button>
       </div>
     </div>
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import { select, geoPath, geoMercator } from "d3";
 import useResizeObserver from "../useResizeObserver";
 import CountryModal from "./CountryModal";
+import axios, { AxiosError } from "axios";
 
 const continentColors = {
   Africa: "#f4a582",
@@ -18,10 +19,28 @@ const WorldMap = ({ data }) => {
   const dimensions = useResizeObserver(wrapperRef);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [countryDetails, setCountryDetails] = useState(null);
+  const [errorMessage,setErrorMessage]=useState(null);
 
   const handleModalClose = () => {
     setSelectedCountry(null);
     setShowModal(false); 
+  };
+  const openModal = async (feature) => {
+    try{
+    const response= await axios.get(`https://restcountries.com/v3.1/name/${feature.properties.name}`);
+   
+   if(response?.data.length>0){
+    const countryData=response?.data.filter((country) => country?.name?.common === feature?.properties?.name);
+    setCountryDetails(countryData?.[0]);
+    setSelectedCountry(feature);
+    setShowModal(true);
+   }
+    }
+    catch(error){
+      const errorMessage = error;
+      setErrorMessage(errorMessage);
+    }
   };
   useEffect(() => {
     const svg = select(svgRef.current);
@@ -37,10 +56,8 @@ const WorldMap = ({ data }) => {
       .selectAll(".country")
       .data(data.features)
       .join("path")
-      .on("click", (event, feature) => {
-        setSelectedCountry(feature);
-        setShowModal(true); 
-      })
+      .on("click", (event, feature) => openModal(feature)
+       )
       .attr("class", "country")
       .transition()
       .attr("fill", (feature) => {
@@ -79,7 +96,10 @@ const WorldMap = ({ data }) => {
         show={showModal}
         onClose={handleModalClose} 
         country={selectedCountry}
+        countryDetails={countryDetails}
+        error={errorMessage}
       />
+
     </div>
   );
 };
